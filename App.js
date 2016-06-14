01cdb3a7ea81ab6e6cb66dc9809ff9f1f99c6771
Nick Katarow <nick@turnstylestudio.com>
2016-06-14 12:12:41 -0700

    video 6

---
+++ App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// This can have states
+class App extends React.Component {
+	constructor(){
+		super();
+		this.state = { txt: 'this is the state txt' }
+		this.update = this.update.bind(this);
+	}
+	update(event) {
+		this.setState({ txt: event.target.value })
+	}
+	render(){
+		let txt = this.props.txt
+		return (
+			<div>
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+				<Widget txt={this.state.txt} update={this.update} />
+			</div>
+		);
+	}
+}
+
+const Widget = (props) => {
+	return (
+		<div>
+			<input type="text" onChange={props.update} />
+			<h1>{props.txt}</h1>
+		</div>
+	);
+}
+
+
+// This is cannot have states
+// const App = () => <h1>Hellow Eggheads</h1>
+
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt: 'Default txt'
+}
+
+ReactDOM.render(
+	<App cat={5} txt="this is the props value" />,
+	document.getElementById('app')
+);
+
+//export default App
